f250a5a447df3a358c0b8f21f38bcdce16e3dcbc
Mauro Rossi <issor.oruam@gmail.com>
2020-07-25 01:02:49 +0200

    android: AMDGPU/GlobalISel: Introduce post-legalize combiner
    android: AMDGPU/GlobalISel: Introduce post-legalize combiner
    
    Port to Android.bp of commit
    fee41517fe0 ("AMDGPU/GlobalISel: Introduce post-legalize combiner")
    
    Fixes the following build error:
    
    external/llvm-project/llvm/lib/Target/AMDGPU/AMDGPUPostLegalizerCombiner.cpp:229:10:
    fatal error: 'AMDGPUGenPostLegalizeGICombiner.inc' file not found
             ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    1 error generated.
    
    Signed-off-by: Mauro Rossi <issor.oruam@gmail.com>

--- llvm/soong/tblgen.go
+++ llvm/soong/tblgen.go
@@ -145,6 +145,8 @@ func outToGenerator(ctx android.ModuleContext, out string) string {
 		return "-gen-global-isel-combiner -combiners=\"AArch64PostLegalizerCombinerHelper\""
 	case strings.HasSuffix(out, "AMDGPUGenPreLegalizeGICombiner.inc"):
 		return "-gen-global-isel-combiner -combiners=\"AMDGPUPreLegalizerCombinerHelper\""
+	case strings.HasSuffix(out, "AMDGPUGenPostLegalizeGICombiner.inc"):
+		return "-gen-global-isel-combiner -combiners=\"AMDGPUPostLegalizerCombinerHelper\""
 	case strings.HasSuffix(out, "GenGlobalISel.inc"):
 		return "-gen-global-isel"
 	case strings.HasSuffix(out, "X86GenEVEX2VEXTables.inc"):
