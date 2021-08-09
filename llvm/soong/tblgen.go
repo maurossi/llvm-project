f0aeae9369457475f8c49f54cfd74155b4ea8534
Mauro Rossi <issor.oruam@gmail.com>
2021-08-09 19:53:12 +0200

    android: [openacc][openmp] update soong building rules
    Changes due to following commits:
    
    0a90ffa ("[flang][openacc] OpenACC 3.0 parser")
    d709dcc ("[openacc][openmp] Reduce number of generated file and prefer inclusion of .inc")

--- llvm/soong/tblgen.go
+++ llvm/soong/tblgen.go
@@ -191,11 +191,13 @@ func outToGenerator(ctx android.ModuleContext, out string) string {
 		return "-gen-intrinsic-enums -intrinsic-prefix=xcore"
 	case out == "IntrinsicsVE.h":
 		return "-gen-intrinsic-enums -intrinsic-prefix=ve"
+	case out == "ACC.h.inc":
+		return "--gen-directive-decl"
+	case out == "ACC.inc":
+		return "--gen-directive-impl"
 	case out == "OMP.h.inc":
 		return "--gen-directive-decl"
-	case out == "OMP.cpp.inc":
-		return "--gen-directive-gen"
-	case out == "OMP.cpp":
+	case out == "OMP.inc":
 		return "--gen-directive-impl"
 	}
 
