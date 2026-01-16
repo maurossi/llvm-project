c11b204470a8cd42ea5e86806731f1b610b6c044
Mauro Rossi <issor.oruam@gmail.com>
2026-01-16 17:21:57 -0500

    android: [X86][GISEL] Enable Pre Legalizer Combiner
    Porting of the following commit:
    
    05a34dd "[X86][GISEL] Enable Pre Legalizer Combiner"

--- llvm/soong/tblgen.go
+++ llvm/soong/tblgen.go
@@ -169,6 +169,8 @@ func outToGenerator(ctx android.ModuleContext, out string) string {
 		return "-gen-x86-mnemonic-tables -asmwriternum=1"
 	case strings.HasSuffix(out, "X86GenFoldTables.inc"):
 		return "-gen-x86-fold-tables -asmwriternum=1"
+	case strings.HasSuffix(out, "X86GenPreLegalizeGICombiner.inc"):
+		return "-gen-global-isel-combiner -combiners=\"X86PreLegalizerCombiner\""
 	case out == "Attributes.inc", out == "AttributesCompatFunc.inc":
 		return "-gen-attrs"
 	case out == "IntrinsicEnums.inc":
